e4924f79963faff902989cf7b15917adae8c5479
xiao <guxinsuiyuexing@163.com>
2018-08-21 11:34:45 +0800

    lizi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,13 @@
   <div class="hello">
    <p @click="btn" class="tankuang">点我啊</p>
    <p>dffdsf</p>
-   <p @click="demo" class="tankuang">试试看</p>
+   <p @click="demos" class="tankuang">试试看</p>
+   <Demoblock
+      :background="demo.background"
+      :text="demo.text"
+      :button-styles="demo.buttonStyles"
+      :button-options="demo.buttonOptions"
+   />
    <p>dffdsf</p>
    <p>dffdsf</p>
    <p>dffdsf</p>
@@ -22,18 +28,34 @@
 </template>
 
 <script>
+import Demoblock from '../demo-block'
 export default {
   name: 'HelloWorld',
+  components: {
+    Demoblock
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      demo: {
+        background: 'linear-gradient(120deg, #a8edea 0%, #fed6e3 100%)',
+        text: 'Send',
+        buttonStyles: {
+          background: '#121019',
+          color: '#fff'
+        },
+        buttonOptions: {
+          particleColor: '#121019',
+          duration: 2000
+        }
+      }
     }
   },
   methods: {
     btn () {
       this.$edit.open('index', 'zaia', {}, {show: true, width: '40%'})
     },
-    demo () {
+    demos () {
       var obj = {
         name: 'zhangsan',
         age: 18
